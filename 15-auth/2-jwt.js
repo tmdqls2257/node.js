55281febf60ea9b6758ffcdea473e37b61dffdee
tmdqls2257 <54207332+tmdqls2257@users.noreply.github.com>
2021-12-11 00:03:48 +0900

    update

--- 15-auth/2-jwt.js
+++ 15-auth/2-jwt.js
@@ -4,13 +4,13 @@ const jwt = require('jsonwebtoken')
 const secret = 'fSTWh2471^%Vw9dmUyYR$BXL*VJhq&N&'
 const token = jwt.sign(
   {
-    // payload
+    // payload 담고싶은 데이터를 담으면 됨
     id: 'ellie',
     isAdmin: false,
   },
   secret,
   // 2초후에 만료가 됨
-  { expiresIn: 2 }
+  { expiresIn: 5 }
 )
 
 // 3초후에 작동하기 때문에 expire되었다고 오류가 남
--- test/app.js
+++
@@ -1,8 +0,0 @@
-// 계획
-// 1. 사용자가 원하는 폴더의 이름을 받아온다.
-// 2. 그 폴더안에 video, captured, duplicated 폴더를 만든다.
-// 3. 폴더 안에 있는 파일들을 다 돌면서 해당하는 비디오, 이미지,
-
-const fs = require('fs')
-console.log(process.argv)
-// node app.js test 입력하면 test가 나옴
