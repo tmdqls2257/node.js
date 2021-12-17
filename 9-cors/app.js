e32fa09b487d7404fee7be6d1e0284b865932d5d
tmdqls2257 <54207332+tmdqls2257@users.noreply.github.com>
2021-12-17 23:21:20 +0900

    update

--- 9-cors/app.js
+++ 9-cors/app.js
@@ -1,7 +1,11 @@
 import express from 'express'
 import cors from 'cors'
+import cookieParser from 'cookie-parser'
 
 const app = express()
+
+app.use(express.json())
+app.use(cookieParser())
 
 app.use(
   cors({
